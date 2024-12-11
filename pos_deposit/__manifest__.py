e66311ad0f1ed0ad40f45df36866476e0e4987a4
ntsirintanis <ntsirintanis@therp.nl>
2024-12-11 15:03:40 +0100

    [MIG] pos_deposit: Migration to 16.0

--- pos_deposit/__manifest__.py
+++ pos_deposit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "POS Container Deposit",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Point of Sale",
     "summary": "This module is used to manage container deposits for products"
     " in Point of Sale.",
@@ -8,7 +8,10 @@
     "website": "https://github.com/OCA/pos",
     "license": "AGPL-3",
     "depends": ["point_of_sale"],
-    "data": ["views/pos_deposit.xml", "views/product_view.xml"],
-    "qweb": ["static/src/xml/pos.xml"],
+    "data": ["views/product_view.xml"],
+    "assets": {
+        "web.assets_backend": ["/pos_deposit/static/src/js/pos.js"],
+        "web.assets_qweb": ["/pos_deposit/static/src/xml/pos.xml"],
+    },
     "installable": True,
 }
