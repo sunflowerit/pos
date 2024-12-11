3ce8f278f7512281b5a11d8514a2e155180e05a6
ntsirintanis <ntsirintanis@therp.nl>
2024-12-11 14:34:02 +0100

    [IMP] pos_deposit: pre-commit stuff

--- pos_deposit/static/src/js/pos.js
+++ pos_deposit/static/src/js/pos.js
@@ -1,21 +1,21 @@
 /* Copyright 2021 Sunflower IT
    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 
-odoo.define("pos_deposit.models", function(require) {
+odoo.define("pos_deposit.models", function (require) {
     "use strict";
     var models = require("point_of_sale.models");
 
     var pos_model_super = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
-        initialize: function() {
+        initialize: function () {
             /* Add the deposit related fields to the product model */
             var self = this;
-            _.each(self.models, function(model) {
+            _.each(self.models, function (model) {
                 if (model.model === "product.product") {
                     model.fields.push("select_deposit");
                     model.fields.push("use_deposit");
                     var domain_super = model.domain;
-                    model.domain = function(arg) {
+                    model.domain = function (arg) {
                         var domain = domain_super(arg);
                         domain.unshift(["is_deposit", "=", true]);
                         domain.unshift("|");
@@ -33,11 +33,11 @@ odoo.define("pos_deposit.models", function(require) {
            point_of_sale's add_product() has triggered a merge with an existing
            line, and in that case don't do anything. Otherwise, add a line
            with the deposit product. */
-        add_product: function(product) {
+        add_product: function (product) {
             order_super.add_product.apply(this, arguments);
 
             var merged_line = null;
-            this.get_orderlines().some(function(orderline) {
+            this.get_orderlines().some(function (orderline) {
                 // TB: merged flag is set by us
                 if (orderline.merged) {
                     merged_line = orderline;
@@ -71,13 +71,13 @@ odoo.define("pos_deposit.models", function(require) {
 
     var order_line_super = models.Orderline.prototype;
     models.Orderline = models.Orderline.extend({
-        merge: function() {
+        merge: function () {
             /* Mark the target merge line to be picked up in our override of
                addProduct() */
             order_line_super.merge.apply(this, arguments);
             this.merged = 1;
         },
-        set_quantity: function(quantity, no_propagate) {
+        set_quantity: function (quantity, no_propagate) {
             /* Keep quantities in sync between deposit product lines and
                lines containing the deposit */
             order_line_super.set_quantity.apply(this, arguments);
